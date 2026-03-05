89f299bbdbb054dbecd89dceef5ba63405feaa58
aditya-rawat86 <rawatsunny747@gmail.com>
2026-03-05 13:32:59 +0530

    code

---
+++ Day 28/55_circular_list.c
@@ -0,0 +1,58 @@
+/*Problem: Circular Linked List Creation and Traversal - Implement using linked list with dynamic 
+memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the circular linked list elements starting from head, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+
+Explanation:
+Last node's next points to head. Traverse from head until returning to head to avoid infinite loop.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node *next;
+};
+int main(){
+    int n;
+    scanf("%d",&n);
+    struct node *head=NULL,*temp,*newnode;
+    for(int i=0;i<n;i++){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+        if(head==NULL){
+            head=newnode;
+            temp=newnode;
+        }
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+    temp->next=head;
+    temp=head;
+    do
+    {
+        printf("%d",temp->data);
+        temp=temp->next;
+        if(temp!=head){
+            printf(" ");
+        }
+    }while(temp!=head);
+    return 0;
+}
