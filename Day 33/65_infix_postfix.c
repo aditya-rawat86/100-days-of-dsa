c6e01298a9d9c47891b3779cf2149b175b6eaf60
aditya-rawat86 <rawatsunny747@gmail.com>
2026-03-15 12:00:18 +0530

    code

---
+++ Day 33/65_infix_postfix.c
@@ -0,0 +1,73 @@
+/*Problem: Convert an infix expression to postfix notation using stack.
+
+Input:
+- Single line: infix expression (operands are single characters)
+
+Output:
+- Print the postfix expression
+
+Example:
+Input:
+A+B*C
+
+Output:
+ABC*+
+
+Explanation:
+Operator precedence: * > +
+Use stack to handle operator precedence and associativity*/
+
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 100
+char stack[MAX];
+int top = -1;
+void push(char x)
+{
+    stack[++top] = x;
+}
+char pop()
+{
+    return stack[top--];
+}
+char peek()
+{
+    return stack[top];
+}
+int precedence(char op)
+{
+    if(op == '+' || op == '-')
+        return 1;
+    if(op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+int main()
+{
+    char infix[100], postfix[100];
+    int k = 0;
+    scanf("%s", infix);
+    for(int i = 0; infix[i] != '\0'; i++)
+    {
+        char ch = infix[i];
+        if(isalnum(ch))
+        {
+            postfix[k++] = ch;
+        }
+        else
+        {
+            while(top != -1 && precedence(peek()) >= precedence(ch))
+            {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+    while(top != -1)
+    {
+        postfix[k++] = pop();
+    }
+    postfix[k] = '\0';
+    printf("%s", postfix);
+    return 0;
+}
