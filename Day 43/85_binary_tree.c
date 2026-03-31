bfa3ef8edca910bec7aa3c943213b804d88a7ac2
aditya-rawat86 <rawatsunny747@gmail.com>
2026-03-31 19:48:35 +0530

    code

---
+++ Day 43/85_binary_tree.c
@@ -0,0 +1,84 @@
+/*Problem Statement:
+Construct a Binary Tree from the given level-order traversal.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers (-1 represents NULL)
+
+Output Format:
+- Print inorder traversal of the constructed tree
+
+Example:
+Input:
+7
+1 2 3 4 5 -1 6
+
+Output:
+4 2 5 1 3 6*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{int data;struct node*left,*right;};
+
+struct node* newnode(int x)
+{
+    struct node* t=(struct node*)malloc(sizeof(struct node));
+    t->data=x;t->left=t->right=NULL;
+    return t;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+
+    if(n==0||a[0]==-1) return 0;
+
+    struct node* q[n];
+    int f=0,r=0;
+
+    struct node* root=newnode(a[0]);
+    q[r++]=root;
+
+    int i=1;
+    while(i<n)
+    {
+        struct node* cur=q[f++];
+
+        if(a[i]!=-1)
+        {
+            cur->left=newnode(a[i]);
+            q[r++]=cur->left;
+        }
+        i++;
+
+        if(i<n && a[i]!=-1)
+        {
+            cur->right=newnode(a[i]);
+            q[r++]=cur->right;
+        }
+        i++;
+    }
+
+    struct node* stack[100];
+    int top=-1;
+    struct node* curr=root;
+
+    while(curr||top!=-1)
+    {
+        while(curr)
+        {
+            stack[++top]=curr;
+            curr=curr->left;
+        }
+        curr=stack[top--];
+        printf("%d",curr->data);
+        if(curr->right||top!=-1) printf(" ");
+        curr=curr->right;
+    }
+
+    return 0;
+}
